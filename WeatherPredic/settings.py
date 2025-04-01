5150fc9ca1a89021497322ac191546ab3ad7827a
Prakhar Singhwal <x23285435@student.ncirl.ie>
2025-04-01 01:16:48 +0100

    changes in settings.py

--- WeatherPredic/settings.py
+++ WeatherPredic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5esgtomarm#@0!uik@%d_44^!cd42faruk437w)ef+@g@3m+ek-4l@g_l6o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.216.134.233', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['108.129.212.27', 'localhost', '127.0.0.1']
 
 
 
